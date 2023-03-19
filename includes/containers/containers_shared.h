c7ff86562b00a19b15d4a9be0d620f0efdaf26bb
Steven Sudit <stevensudit@gmail.com>
2023-03-19 16:20:47 -0400

    Fix transparent collections and cover unsorted

--- includes/containers/containers_shared.h
+++ includes/containers/containers_shared.h
@@ -19,3 +19,7 @@
 #include "../meta.h"
 
 #include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
--- includes/containers/transparent.h
+++ includes/containers/transparent.h
@@ -18,21 +18,15 @@
 #pragma once
 #include "containers_shared.h"
 
-namespace covid {
+namespace corvid {
 inline namespace containers {
 
-// The transparent comparator allows comparing any stringlike values without
-// constructing temporary `std::string` instances from them.
+// Transparent comparators allowscomparing any stringlike values without
+// constructing temporary `std::string` instances from them. This works for
+// `find` but not `operator[]`.
 //
-// By specifying it as the `less<>` replacement in a container that supports
-// `is_transparent` (currently `std::map` and `std::set`, but also
-// `std::unordered_set` and `std::unordered_map`, starting in C++20), methods
-// such as `find` take advantage of this copy-free comparison.
-//
-// The `string_map<V>` and `string_set` aliases do this for you.
-//
-// The comparator should also work for collections keyed on anything else that
-// can be cast to `std::string_view`.
+// For ordered collections, we need to define a `less`. For unordered, we need
+// `hash` and `equal`.
 //
 // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2018/p0919r3.html
 struct transparent_less_stringlike {
@@ -44,6 +38,25 @@ struct transparent_less_stringlike {
   }
 };
 
+struct transparent_hash_stringlike {
+  using is_transparent = void;
+
+  template<typename T>
+  constexpr size_t operator()(const T& t) const {
+    return std::hash<std::string_view>{}(static_cast<std::string_view>(t));
+  }
+};
+
+struct transaprent_equal_stringlike {
+  using is_transparent = void;
+
+  template<typename T, typename V>
+  constexpr bool operator()(const T& l, const V& r) const {
+    return static_cast<std::string_view>(l) ==
+           static_cast<std::string_view>(r);
+  }
+};
+
 // Map keyed by `std::string`, with transparent search.
 template<typename V = std::string,
     typename A = std::allocator<std::pair<const std::string, V>>>
@@ -51,9 +64,17 @@ using string_map = std::map<std::string, V, transparent_less_stringlike, A>;
 
 // Set of `std::string`, with transparent search.
 template<typename A = std::allocator<std::string>>
-using string_set = std::map<std::string, transparent_less_stringlike, A>;
+using string_set = std::set<std::string, transparent_less_stringlike, A>;
 
-// TODO: Add unsorted_map and unsorted_set.
+// Unordered map keyed by `std::string`, with transparent search.
+template<typename V = std::string>
+using string_unordered_map = std::unordered_map<std::string, V,
+    transparent_hash_stringlike, transaprent_equal_stringlike>;
+
+// Unordered set of `std::string`, with transparent search.
+template<typename A = std::allocator<std::string>>
+using string_unordered_set = std::unordered_set<std::string,
+    transparent_hash_stringlike, transaprent_equal_stringlike>;
 
 } // namespace containers
-} // namespace covid
+} // namespace corvid
