c504bbf502392681e749cb13c6ed5be539d7ed9a
Steven Sudit <stevensudit@gmail.com>
2025-07-03 00:05:39 -0400

    Clean up enum variant

--- corvid/containers/enum_variant.h
+++ corvid/containers/enum_variant.h
@@ -1,3 +1,19 @@
+// Corvid: A general-purpose modern C++ library extending std.
+// https://github.com/stevensudit/Corvid
+//
+// Copyright 2022-2025 Steven Sudit
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #pragma once
 #include <variant>
 
@@ -238,7 +254,10 @@ public:
   // Note that, being implicit, this lets you assign an `enum_type` to an
   // `enum_variant`.
   consteval enum_variant(enum_type e)
-      : value_(construct(static_cast<std::size_t>(e))) {}
+      : value_(construct(static_cast<std::size_t>(e))) {
+    static_assert(static_cast<std::size_t>(e) < variant_size,
+        "Enum index out of range");
+  }
 
   // Conversion constructor, where `T` is one of the types in `Ts...`.
   template<typename T>
