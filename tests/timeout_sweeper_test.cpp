e7b53cedcde79f428b21e9a4e8881ffbcfb01af9
Steven Sudit <stevensudit@gmail.com>
2026-05-29 18:22:07 -0400

    checkpoint

--- tests/timeout_sweeper_test.cpp
+++ tests/timeout_sweeper_test.cpp
@@ -17,6 +17,7 @@
 
 #include "../corvid/concurrency/timeout_sweeper.h"
 #include "../corvid/meta/fixed_function.h"
+#include "../corvid/infra.h"
 
 #include "catch2_main.h"
 
@@ -25,6 +26,7 @@
 
 using namespace std::chrono_literals;
 using namespace corvid::concurrency;
+using namespace corvid::infra;
 
 using sweeper = timeout_sweeper<>;
 using tp = sweeper::time_point_t;
